6e0c55585b57ba0f25a4d6d8dfb6da058981194c
abdulaziz-maker <abdulaziz1ayman@gmail.com>
2025-11-05 13:34:21 +0300

    Update answer.sql
    test the query

--- answer.sql
+++ answer.sql
@@ -1,1 +1,16 @@
--- ADD YOUR SQL QUERY HERE
+SELECT 
+    t.investor_id,
+    s.sector_name,
+    ROUND((t.no_of_shares * 100.0) / totals.total_shares, 2) AS percentage
+FROM investor_transactions t
+JOIN (
+    SELECT investor_id, SUM(no_of_shares) AS total_shares
+    FROM investor_transactions
+    GROUP BY investor_id
+) totals
+    ON t.investor_id = totals.investor_id
+JOIN sectors s
+    ON t.sector_id = s.sector_id
+ORDER BY 
+    t.investor_id,
+    t.sector_id;
